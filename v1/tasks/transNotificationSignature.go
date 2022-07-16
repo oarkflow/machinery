e127902e448fe3ac5268a0cb904fa203a24cd8c8
baikang.lu <baikang.lu@cardinfolink.com>
2022-07-16 12:28:11 +0800

    add biz field

--- v1/tasks/transNotificationSignature.go
+++ v1/tasks/transNotificationSignature.go
@@ -26,12 +26,13 @@ type TransNotification struct {
 	IntStoreCode string `bson:"intStoreCode,omitempty"`
 	EvoTransID   string `bson:"evoTransID,omitempty"`
 	// MsgTye      int    `bson:"msgTye,omitempty"`      // 0-异步通知 1-Push通知 2-Email通知 3-SMS通知
-	FromChanMsg string `bson:"fromChanMsg,omitempty"` // 渠道异步消息报文（内部字段）
-	ToMerMsg    string `bson:"toMerMsg,omitempty"`    // 对下异步通知内容（内部字段）
-	SendTimes   int    `bson:"sendTimes,omitempty"`   // 发送次数
-	Destination string `bson:"destination,omitempty"` // 消息投递目的地
-	ProductCode string `bson:"productCode,omitempty"`
-	TraceID     string `bson:"traceID,omitempty"`
+	FromChanMsg      string `bson:"fromChanMsg,omitempty"` // 渠道异步消息报文（内部字段）
+	ToMerMsg         string `bson:"toMerMsg,omitempty"`    // 对下异步通知内容（内部字段）
+	SendTimes        int    `bson:"sendTimes,omitempty"`   // 发送次数
+	Destination      string `bson:"destination,omitempty"` // 消息投递目的地
+	ProductCode      string `bson:"productCode,omitempty"`
+	TraceID          string `bson:"traceID,omitempty"`
+	NotificationType string `bson:"notificationType,omitempty"`
 }
 
 // trans notification signature
